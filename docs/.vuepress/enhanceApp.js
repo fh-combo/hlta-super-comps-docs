2a2ec8d3e0fbca39aadc44a36f49de9938fa7c2f
fhcombo <fh95sky@126.com>
2022-11-16 11:39:50 +0800

    123

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,18 +1,23 @@
-import Vue from 'vue';
+// import Vue from 'vue';
 export default ({
   router
 }) => {
   router.beforeEach((to, from, next) => {
-    next();
-    setTimeout(() => {
+    // setTimeout(() => {
       // let tables = document.querySelectorAll('#app > .theme-container > .page > .content > table');
       // tables && Array.from(tables).forEach(table =>{
       //     table.outerHTML = `<div class="table">${table.outerHTML}</div>`;
       // });
       // this.$nextTick(()=>{
-      console.log("hello vue~", $.fancybox);
-      $.fancybox.close()
+      console.log("hello vue~", $.fancybox.getInstance());
+      const fancyInstance = $.fancybox.getInstance()
+      if (fancyInstance && fancyInstance.isVisible) {
+        $.fancybox.close()
+        next();
+      }
+      // $.fancybox.close()
       // })
-    })
+    // })
+    next();
   })
 };
